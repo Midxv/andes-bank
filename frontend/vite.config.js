1380e04a831f1c43c9ebd2473f5db28ca24e6954
asifbm79-star <asifbm79@gmail.com>
2025-12-10 23:10:46 +0530

    Reset base path for custom domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // base: '/andes-bank/',  <-- REMOVE OR COMMENT OUT THIS LINE
+    base: '/', // Set it to root for custom domains
 })
