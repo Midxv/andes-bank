16377a92c27fec0b0de4c2ea7a968fd849548ad2
asifbm79-star <asifbm79@gmail.com>
2025-12-10 23:32:03 +0530

    Fix gitignore and vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // base: '/andes-bank/',  <-- REMOVE OR COMMENT OUT THIS LINE
-    base: '/', // Set it to root for custom domains //ed
-    base: '/', // Set it to root for custom domains //ed
+    base: '/', // <--- MUST BE '/' FOR VERCEL / CUSTOM DOMAINS
 })
